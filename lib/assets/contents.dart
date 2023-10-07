154fee89e258b979f56ad4d6d4965b5a118a61aa
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2023-10-07 15:41:42 +0530

    se

--- lib/assets/contents.dart
+++ lib/assets/contents.dart
@@ -5,21 +5,24 @@ import 'package:flutter/material.dart';
 Widget contents(BuildContext context){
   return Column(
     children: [
-      Container(
+      SizedBox(
+        height: MediaQuery.of(context).size.height/64,
+      )
+      ,Container(
         height: MediaQuery.of(context).size.height/2.5,
         decoration: BoxDecoration(
-          color: Colors.black
+          color: Colors.transparent
           
         ),
-      ),
-      SizedBox(
-        height: MediaQuery.of(context).size.height/60,
+        child: Center(child: Image.asset('lib/assets/Picsart_23-10-06_22-56-38-205.png')),
       ),
       Container(
         height: MediaQuery.of(context).size.height/2.5,
+        width: double.infinity,
         decoration: BoxDecoration(
-          color: Colors.black
-        )
+          color: Colors.transparent
+        ),
+        child: Center(child: Text('Example text')),
       )
     ],
   );
--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:se/assets/contents.dart';
+import 'package:video_player/video_player.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,15 +12,75 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+   late VideoPlayerController cntrl1;
+   
+   @override
+  void initState(){
+    super.initState();
+    cntrl1 = VideoPlayerController.asset('lib/assets/background.mp4')
+    ..initialize().then((_){
+      setState(() {
+        
+      });
+      cntrl1.play();
+      cntrl1.setLooping(true);
+      cntrl1.setVolume(0.0);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: contents(context),
+      body: Container(
+        child: Stack(
+          children: [
+            Center(
+                child: cntrl1.value.isInitialized? AspectRatio(  
+                aspectRatio: 0.489,
+                child: VideoPlayer(cntrl1),
+                ):Container(),
+            )
+          ],
         ),
-      )),
+      ),
     );
+  }
+}
+
+class ElevatorApp extends StatefulWidget {
+  @override
+  _ElevatorAppState createState() => _ElevatorAppState();
+}
+
+class _ElevatorAppState extends State<ElevatorApp> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Add a post-frame callback to scroll to the bottom immediately after the build is complete.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: _scrollController,
+      reverse: false, // Reverse the list so it starts at the bottom.
+      itemCount: 50, // Replace with the number of floors in your elevator.
+      itemBuilder: (BuildContext context, int index) {
+        return contents(context);
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
